68c7ebc0caea78c758d70646ce0eddef7c680711
zf1115 <zf1115@skbl-422-010.ph.ic.ac.uk>
2019-01-11 10:40:11 +0000

    Changed compared value to PORT D

--- Simple1.asm
+++ Simple1.asm
@@ -7,13 +7,15 @@
 	org 0x100		    ; Main code starts here at address 0x100
 
 start
+	movlw	0xff
+	movwf	TRISD, ACCESS
 	movlw 	0x0
 	movwf	TRISC, ACCESS	    ; Port C all outputs
 	bra 	test
 loop	movff 	0x06, PORTC
 	incf 	0x06, W, ACCESS
 test	movwf	0x06, ACCESS	    ; Test for end of loop condition
-	movlw 	0x10
+	movf	PORTD, W, ACCESS
 	cpfsgt 	0x06, ACCESS
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
