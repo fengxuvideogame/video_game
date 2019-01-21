f652c408e220a4bcae4f770bee175d3bbd270f28
zf1115 <zf1115@skbl-422-010.ph.ic.ac.uk>
2019-01-21 11:54:30 +0000

    Modified for 1 memory D F/F I/O.

--- Simple1.asm
+++ Simple1.asm
@@ -2,33 +2,49 @@
 	
 	code
 	org 0x0
-	goto	setup
+	goto	start
 	
 	org 0x100		    ; Main code starts here at address 0x100
 
-	; ******* Programme FLASH read Setup Code ****  
-setup	bcf	EECON1, CFGS	; point to Flash program memory  
-	bsf	EECON1, EEPGD 	; access Flash program memory
-	goto	start
-	; ******* My data and where to put it in RAM *
-myTable data	"This is just some data"
-	constant 	myArray=0x400	; Address in RAM for data
-	constant 	counter=0x10	; Address of counter variable
-	; ******* Main programme *********************
-start 	lfsr	FSR0, myArray	; Load FSR0 with address in RAM	
-	movlw	upper(myTable)	; address of data in PM
-	movwf	TBLPTRU		; load upper bits to TBLPTRU
-	movlw	high(myTable)	; address of data in PM
-	movwf	TBLPTRH		; load high byte to TBLPTRH
-	movlw	low(myTable)	; address of data in PM
-	movwf	TBLPTRL		; load low byte to TBLPTRL
-	movlw	.22		; 22 bytes to read
-	movwf 	counter		; our counter register
-loop 	tblrd*+			; move one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0	; move read data from TABLAT to (FSR0), increment FSR0	
-	decfsz	counter		; count down to zero
-	bra	loop		; keep going until finished
+start
+	movlw	0x0
+	movwf	TRISD, ACCESS
+	movwf	TRISE, ACCESS
+	; Initialization Address Bus
+	movlw	0x0f		    ; Set W to 1111b
+	movwf	PORTD, ACCESS	    ; Set OE1*, CP1, OE2*, CP2 to HIGH
+	; Set Data at PortD (Data Bus)
+	movlw	0xaa		    ; Set W to 10101010b
+	movwf	PORTE, ACCESS	    ; Move W to BankD
+	; Set CP1 to LOW to high
+	movlw	1101b
+	movwf	PORTD, ACCESS
+	movlw	1111b
+	movwf	PORTD, ACCESS
+	; Set REPU and TRISE
+	banksel	PADCFG1
+	bsf	PADCFG1, REPU, BANKED
+	movlb	0x00
+	setf	TRISE		    ; Tri-state PortE
+	; Set OE1* to low
+	movlw	1110b
+	movwf	PORTD, ACCESS
 	
-	goto	0
-
+	
+	goto 0x0
+	
+	
+	
+	
+	movlw 	0x0
+	movwf	TRISB, ACCESS	    ; Port C all outputs
+	bra 	test
+loop	movff 	0x06, PORTB
+	incf 	0x06, W, ACCESS
+test	movwf	0x06, ACCESS	    ; Test for end of loop condition
+	movlw 	0x63
+	cpfsgt 	0x06, ACCESS
+	bra 	loop		    ; Not yet finished goto start of loop again
+	goto 	0x0		    ; Re-run program from start
+	
 	end
