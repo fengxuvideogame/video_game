071a7808938f520a0c99657ff044c02742f67369
zf1115 <zf1115@skbl-422-010.ph.ic.ac.uk>
2019-01-21 15:01:46 +0000

    Implemented memory 1 and 2 using octal D F/F's for I/O test.

--- Simple1.asm
+++ Simple1.asm
@@ -11,40 +11,38 @@ start
 	movwf	TRISD, ACCESS
 	movwf	TRISE, ACCESS
 	; Initialization Address Bus
-	movlw	0x0f		    ; Set W to 1111b
-	movwf	PORTD, ACCESS	    ; Set OE1*, CP1, OE2*, CP2 to HIGH
-	; Set Data at PortD (Data Bus)
+	call	enableadd
+	; Set Data1 at PortD (Data Bus)
 	movlw	0xaa		    ; Set W to 10101010b
 	movwf	PORTE, ACCESS	    ; Move W to BankD
-	; Set CP1 to LOW to high
-	movlw	1101b
+	; Set CP1 to LOW to HIGH
+	movlw	0x0d		    ; 1101
 	movwf	PORTD, ACCESS
-	movlw	1111b
+	call	enableadd
+	; Set Data2 at PortD (Data Bus)
+	movlw	0x55		    ; Set W to 01010101b
+	movwf	PORTE, ACCESS	    ; Move W to BankD
+	; Set CP2 to LOW to HIGH
+	movlw	0x7		    ; 0111
 	movwf	PORTD, ACCESS
+	call	enableadd
 	; Set REPU and TRISE
 	banksel	PADCFG1
 	bsf	PADCFG1, REPU, BANKED
 	movlb	0x00
 	setf	TRISE		    ; Tri-state PortE
 	; Set OE1* to low
-	movlw	1110b
+	movlw	0x0e		    ; 1110
 	movwf	PORTD, ACCESS
-	
-	
+	; Set OE2* to low
+	movlw	0xb		    ; 1011
+	movwf	PORTD, ACCESS
 	goto 0x0
 	
+enableadd
+	movlw	0x0f		    ; Set W to 1111b
+	movwf	PORTD, ACCESS	    ; Set OE1*, CP1, OE2*, CP2 to HIGH
+	return
 	
-	
-	
-	movlw 	0x0
-	movwf	TRISB, ACCESS	    ; Port C all outputs
-	bra 	test
-loop	movff 	0x06, PORTB
-	incf 	0x06, W, ACCESS
-test	movwf	0x06, ACCESS	    ; Test for end of loop condition
-	movlw 	0x63
-	cpfsgt 	0x06, ACCESS
-	bra 	loop		    ; Not yet finished goto start of loop again
-	goto 	0x0		    ; Re-run program from start
-	
+
 	end
