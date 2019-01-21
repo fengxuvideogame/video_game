ae6bb839e891dd708f7e9efc5bc5f1de2e78ca57
zf1115 <zf1115@skbl-422-010.ph.ic.ac.uk>
2019-01-21 16:44:24 +0000

    Implemented SPI communication

--- Simple1.asm
+++ Simple1.asm
@@ -2,7 +2,7 @@
 	
 	code
 	org 0x0
-	goto	start
+	goto	SPI
 	
 	org 0x100		    ; Main code starts here at address 0x100
 
@@ -45,4 +45,64 @@ enableadd
 	return
 	
 
+
+SPI
+	call	SPI_MasterInit
+	movlw	0x0
+	movwf	0x26	; put in 0x26
+loop	
+	incf	0x26, 0 ; increase by 1
+	movwf	0x26
+	call	SPI_MasterTransmit
+	call	Delay
+	tstfsz	0x26
+	bra	loop
+	goto	0x0
+	
+	
+SPI_MasterInit ; Set Clock edge to negative
+	bcf	SSP2STAT, CKE
+	; MSSP enable; CKP=1; SPI master, clock=Fosc/64 (1MHz)
+	movlw	(1<<SSPEN)|(1<<CKP)|(0x02)
+	movwf	SSP2CON1
+	; SDO2 output; SCK2 output
+	bcf	TRISD, SDO2
+	bcf	TRISD, SCK2
+	return
+	
+SPI_MasterTransmit ; Start transmission of data (held in W)
+	movwf	SSP2BUF	
+Wait_Transmit ; Wait for transmission to complete
+	btfss	PIR2, SSP2IF
+	bra	Wait_Transmit
+	bcf	PIR2, SSP2IF ; clear interrupt flag
+	return
+
+Delay ; Delay
+	movlw	0x0a
+	movwf	0x20
+	call	PrimaryDelay
+	
+PrimaryDelay
+	dcfsnz	0x20
+	return
+	movlw	0xff
+	movwf	0x22
+	call	SecondaryDelay
+	bra	PrimaryDelay
+	
+SecondaryDelay
+	dcfsnz	0x22
+	return
+	movlw	0xff
+	movwf	0x24
+	call	ThirdaryDelay
+	bra	SecondaryDelay
+	
+ThirdaryDelay
+	decfsz	0x24
+	bra	ThirdaryDelay
+	return
+	
+	
 	end
