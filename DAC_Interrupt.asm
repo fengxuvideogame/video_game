996e5d0038b2d0c35284aa1346816c67f8f43139
zf1115 <zf1115@skbl-422-010.ph.ic.ac.uk>
2019-02-04 15:48:08 +0000

    Implemented Sine wave at low freq. (distorted at high freq)

--- DAC_Interrupt.asm
+++ DAC_Interrupt.asm
@@ -3,11 +3,19 @@
 	global	DAC_Setup
 	
 	extern	LCD_delay_ms
+
+acs0	udata_acs
+counter	res 1
 	
 int_hi	code	0x0008	; high vector, no low vector
 	btfss	INTCON,TMR0IF	; check that this is timer0 interrupt
 	retfie	FAST		; if not then return
-	incf	LATD		; increment PORTD
+	;incf	LATD		; increment PORTD
+	tblrd*+
+	movf	TABLAT, W
+	movwf	LATD
+	DCFSNZ	counter
+	call	Sine_Setup
 	clrf	LATC
 	call	LCD_delay_ms
 	movlw	b'00000001'	; Set Write Enable * to HIGH
@@ -17,9 +25,10 @@ int_hi	code	0x0008	; high vector, no low vector
 
 DAC	code
 DAC_Setup
+	call	Sine_Setup
 	clrf	TRISD		; Set PORTD as all outputs
 	clrf	LATD		; Clear PORTD outputs
-	movlw	b'10000000'	; Set timer0 to 16-bit, Fosc/4/256
+	movlw	b'10000110'	; Set timer0 to 16-bit, Fosc/4/256
 	movwf	T0CON	; = 62.5KHz clock rate, approx 1sec rollover
 	bsf	INTCON,TMR0IE	; Enable timer0 interrupt
 	bsf	INTCON,GIE	; Enable all interrupts
@@ -28,5 +37,20 @@ DAC_Setup
 	movwf	PORTC
 	return
 
+sine_table
+	db	0x80, 0x98, 0xb0, 0xc7, 0xda, 0xea, 0xf6, 0xfd, 0xff, 0xfd, 0xf6, 0xea, 0xda, 0xc7, 0xb0, 0x98, 0x80, 0x67, 0x4f, 0x38, 0x25, 0x15, 0x9, 0x2, 0x0, 0x2, 0x9, 0x15, 0x25, 0x38, 0x4f, 0x67
+	constant    sine_table_l=.32
+	
+Sine_Setup
+	movlw	upper(sine_table)	; address of data in PM
+	movwf	TBLPTRU		; load upper bits to TBLPTRU
+	movlw	high(sine_table)	; address of data in PM
+	movwf	TBLPTRH		; load high byte to TBLPTRH
+	movlw	low(sine_table)	; address of data in PM
+	movwf	TBLPTRL		; load low byte to TBLPTRL
+	movlw	sine_table_l	; bytes to read
+	movwf 	counter		; our counter register
+	return
+	
 	end
 
