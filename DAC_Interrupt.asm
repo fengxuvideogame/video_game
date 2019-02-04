065a2952d9fba899c4c311dd999bb0d462d85a93
zf1115 <zf1115@skbl-422-010.ph.ic.ac.uk>
2019-02-04 14:21:11 +0000

    Implemented 477mHz Ramp down signal.

--- DAC_Interrupt.asm
+++ DAC_Interrupt.asm
@@ -2,10 +2,16 @@
 	
 	global	DAC_Setup
 	
+	extern	LCD_delay_ms
+	
 int_hi	code	0x0008	; high vector, no low vector
 	btfss	INTCON,TMR0IF	; check that this is timer0 interrupt
 	retfie	FAST		; if not then return
 	incf	LATD		; increment PORTD
+	clrf	LATC
+	call	LCD_delay_ms
+	movlw	b'00000001'	; Set Write Enable * to HIGH
+	movwf	PORTC
 	bcf	INTCON,TMR0IF	; clear interrupt flag
 	retfie	FAST		; fast return from interrupt
 
@@ -13,11 +19,14 @@ DAC	code
 DAC_Setup
 	clrf	TRISD		; Set PORTD as all outputs
 	clrf	LATD		; Clear PORTD outputs
-	movlw	b'10000111'	; Set timer0 to 16-bit, Fosc/4/256
+	movlw	b'10000000'	; Set timer0 to 16-bit, Fosc/4/256
 	movwf	T0CON	; = 62.5KHz clock rate, approx 1sec rollover
 	bsf	INTCON,TMR0IE	; Enable timer0 interrupt
 	bsf	INTCON,GIE	; Enable all interrupts
+	clrf	TRISC
+	movlw	b'00000001'	; Set Write Enable * to HIGH
+	movwf	PORTC
 	return
-	
+
 	end
 
