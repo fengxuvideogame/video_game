00ef51d68f2856981aa7c9f819fb3bab3d7e154f
zf1115 <zf1115@skbl-319-013.ph.ic.ac.uk>
2019-02-24 11:32:05 +0000

    Compatibility with Delays.asm.

--- LCD.asm
+++ LCD.asm
@@ -1,8 +1,10 @@
 #include p18f87k22.inc
 
-    global  LCD_Setup, LCD_Write_Message, LCD_Send_Byte_I, LCD_delay_x4us
+    global  LCD_setup, LCD_Write_Message, LCD_Send_Byte_I
     global  LCD_Send_Byte_D
-    global  LCD_Write_Hex, LCD_delay_ms
+    global  LCD_Write_Hex
+    
+    extern  delay_ms, delay_x4us, LCD_delay
 
 acs0    udata_acs   ; named variables in access ram
 LCD_cnt_l   res 1   ; reserve 1 byte for variable LCD_cnt_l
@@ -19,36 +21,36 @@ LCD_hex_tmp res 1   ; reserve 1 byte for variable LCD_hex_tmp
 
 LCD	code
     
-LCD_Setup
+LCD_setup
 	clrf    LATB
 	movlw   b'11000000'	    ; RB0:5 all outputs
 	movwf	TRISB
 	movlw   .40
-	call	LCD_delay_ms	; wait 40ms for LCD to start up properly
+	call	delay_ms	; wait 40ms for LCD to start up properly
 	movlw	b'00110000'	; Function set 4-bit
 	call	LCD_Send_Byte_I
 	movlw	.10		; wait 40us
-	call	LCD_delay_x4us
+	call	delay_x4us
 	movlw	b'00101000'	; 2 line display 5x8 dot characters
 	call	LCD_Send_Byte_I
 	movlw	.10		; wait 40us
-	call	LCD_delay_x4us
+	call	delay_x4us
 	movlw	b'00101000'	; repeat, 2 line display 5x8 dot characters
 	call	LCD_Send_Byte_I
 	movlw	.10		; wait 40us
-	call	LCD_delay_x4us
+	call	delay_x4us
 	movlw	b'00001111'	; display on, cursor on, blinking on
 	call	LCD_Send_Byte_I
 	movlw	.10		; wait 40us
-	call	LCD_delay_x4us
+	call	delay_x4us
 	movlw	b'00000001'	; display clear
 	call	LCD_Send_Byte_I
 	movlw	.2		; wait 2ms
-	call	LCD_delay_ms
+	call	delay_ms
 	movlw	b'00000110'	; entry mode incr by 1 no shift
 	call	LCD_Send_Byte_I
 	movlw	.10		; wait 40us
-	call	LCD_delay_x4us
+	call	delay_x4us
 	return
 
 LCD_Write_Hex	    ; Writes byte stored in W as hex
@@ -103,7 +105,7 @@ LCD_Send_Byte_D		    ; Transmits byte stored in W to data reg
 	bsf	LATB, LCD_RS    ; Data write set RS bit	    
         call    LCD_Enable  ; Pulse enable Bit 
 	movlw	.10	    ; delay 40us
-	call	LCD_delay_x4us
+	call	delay_x4us
 	return
 
 LCD_Enable	    ; pulse enable bit LCD_E for 500ns
@@ -125,33 +127,6 @@ LCD_Enable	    ; pulse enable bit LCD_E for 500ns
 	nop
 	bcf	    LATB, LCD_E	    ; Writes data to LCD
 	return
-    
-; ** a few delay routines below here as LCD timing can be quite critical ****
-LCD_delay_ms		    ; delay given in ms in W
-	movwf	LCD_cnt_ms
-lcdlp2	movlw	.250	    ; 1 ms delay
-	call	LCD_delay_x4us	
-	decfsz	LCD_cnt_ms
-	bra	lcdlp2
-	return
-    
-LCD_delay_x4us		    ; delay given in chunks of 4 microsecond in W
-	movwf	LCD_cnt_l   ; now need to multiply by 16
-	swapf   LCD_cnt_l,F ; swap nibbles
-	movlw	0x0f	    
-	andwf	LCD_cnt_l,W ; move low nibble to W
-	movwf	LCD_cnt_h   ; then to LCD_cnt_h
-	movlw	0xf0	    
-	andwf	LCD_cnt_l,F ; keep high nibble in LCD_cnt_l
-	call	LCD_delay
-	return
-
-LCD_delay			; delay routine	4 instruction loop == 250ns	    
-	movlw 	0x00		; W=0
-lcdlp1	decf 	LCD_cnt_l,F	; no carry when 0x00 -> 0xff
-	subwfb 	LCD_cnt_h,F	; no carry when 0x00 -> 0xff
-	bc 	lcdlp1		; carry, then loop again
-	return			; carry reset so return
 
 
     end
