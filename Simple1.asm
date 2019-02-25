8c18c405015ab3d23803b9b2632a7fabbcd7bec9
zf1115 <zf1115@skbl-422-010.ph.ic.ac.uk>
2019-02-25 12:02:24 +0000

    Re-implemented reset vector.

--- Simple1.asm
+++ Simple1.asm
@@ -1,10 +1,17 @@
 	#include p18f87k22.inc
 
-	extern	LED_array_test
+	extern	LED_array_setup
+	extern	high_int_setup
 	
 
+rst	code	0    ; reset vector
+	goto	setup
+	
 main	code
 	; *******  Programme FLASH read Setup Code ***********************
-setup	goto	LED_array_test
-
+setup	
+	call	LED_array_setup
+	call	high_int_setup
+	goto	$
+	
 	end
