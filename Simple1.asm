011b7be62892f72daa47262e4bae876be26d502b
zf1115 <zf1115@skbl-422-010.ph.ic.ac.uk>
2019-02-25 13:07:11 +0000

    LCD and keypad initialisation.

--- Simple1.asm
+++ Simple1.asm
@@ -1,7 +1,9 @@
 	#include p18f87k22.inc
 
+	extern	kpd_ch_setup
 	extern	LED_array_setup
 	extern	high_int_setup
+	extern	LCD_setup, wlcm_display
 	
 
 rst	code	0    ; reset vector
@@ -10,6 +12,9 @@ rst	code	0    ; reset vector
 main	code
 	; *******  Programme FLASH read Setup Code ***********************
 setup	
+	call	LCD_setup
+	call	wlcm_display
+	call	kpd_ch_setup
 	call	LED_array_setup
 	call	high_int_setup
 	goto	$
